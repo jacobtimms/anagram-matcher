5127cd9628a5f508ebf8b11983f249cac4857918
Jacob <jacob_timms@live.com>
2022-01-23 22:59:17 +0000

    anagram matching logic written

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button class="btn" @click="callTransform(inputString)">Test</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      inputString: "apple_car_cider_tar_itch_rat_cried_helicopter_arc.txt",
+    };
+  },
+  methods: {
+    callTransform(input) {
+      console.log(input);
+      input = input.split(".");
+      input.pop();
+      input = input[0].toLowerCase().split("_").sort();
+
+      this.transform(input, []);
+    },
+    transform(wordArray, organisedArray) {
+      console.log(JSON.stringify(wordArray));
+      console.log(JSON.stringify(organisedArray));
+      if (wordArray.length === 0) {
+        return organisedArray;
+      } else {
+        for (let x = 0; x < wordArray.length; x++) {
+          if (wordArray[x] === wordArray[0]) {
+            continue;
+          } else if (wordArray[0].length === wordArray[x].length) {
+            let comparedStrings = this.compareStrings(wordArray[0], wordArray[x]);
+
+            if (comparedStrings !== null) {
+              let pairExists = this.doesPairExist(comparedStrings, organisedArray);
+
+              if (pairExists === false) {
+                organisedArray.push(comparedStrings.sort());
+                wordArray.splice(0, 1);
+                wordArray.splice(x - 1, 1);
+                break;
+              }
+            }
+          }
+          organisedArray.push([wordArray[0]]);
+          wordArray.splice(0, 1);
+          break;
+        }
+        this.transform(wordArray, organisedArray);
+      }
+    },
+    compareStrings(baseString, comparedString) {
+      for (let i = 0; i < baseString.length; i++) {
+        if (!comparedString.split("").includes(baseString[i])) {
+          return null;
+        }
+      }
+      return [baseString, comparedString];
+    },
+    doesPairExist(newArray, existingArray) {
+      let newArrayString = JSON.stringify(newArray.sort());
+
+      existingArray.forEach((array) => {
+        let existingArrayString = JSON.stringify(array.sort());
+
+        if (newArrayString === existingArrayString) {
+          return true;
+        }
+      });
+      return false;
+    },
+  },
+};
 </script>
 
 <style>
+.btn {
+  height: 2rem;
+  width: 10rem;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
